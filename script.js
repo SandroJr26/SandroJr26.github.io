a08701ad0357dc764044e91d5d48c7622047b105
Sandro Junior <nunet26@gmail.com>
2023-02-20 05:29:19 -0300

    btn-dark-mode

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function cursos() {
   const items = document.getElementsByClassName("curso-secundario");
+  const itemsArray = Array.from(items);
   let isVisible = false;
 
-  for (let i = 0; i < items.length; i++) {
-    if (items[i].classList.contains("hidden")) {
+  for (let i = 0; i < itemsArray.length; i++) {
+    if (itemsArray[i].classList.contains("hidden")) {
       isVisible = true;
     }
-    items[i].classList.toggle("hidden");
+    itemsArray[i].classList.toggle("hidden");
   }
 
   const btn = document.querySelector("button.maisCursos");
@@ -16,3 +17,10 @@ function cursos() {
     btn.innerHTML = "Ver Mais";
   }
 }
+
+const btnDarkMode = document.getElementById("btn-dark-mode");
+
+function darkMode(event) {
+  document.body.classList.toggle("dark-mode", event.target.checked);
+}
+btnDarkMode.addEventListener("change", darkMode);
